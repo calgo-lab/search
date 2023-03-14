5c498249f75bb7f6141a87aef1e8f13879724991
AdriaSG <adrianasg.15@gmail.com>
2023-03-14 17:16:38 +0100

    Update utils

--- demo/utils.py
+++ demo/utils.py
@@ -3,7 +3,6 @@ import pickle
 import string
 import zipfile
 
-import pandas as pd
 import streamlit as st
 from sentence_transformers import CrossEncoder, SentenceTransformer
 from sklearn.feature_extraction import _stop_words
@@ -45,67 +44,8 @@ def get_bm25():
         with myzip.open("bm25_corpus_embeddings.p", "r") as f:
             return pickle.load(f)
 
-
-# Load the pickled bi-encoder sbert embeddings
-@st.cache_resource(show_spinner="Fetching SBERT embeddings...")
-def get_sbert_embeddings():
-    with zipfile.ZipFile(
-        os.path.join(
-            dir_path, "multi-qa-mpnet-base-dot-v1_greendb_corpus_embeddings.p.zip"
-        ),
-        "r",
-    ) as myzip:
-        with myzip.open(
-            "multi-qa-mpnet-base-dot-v1_greendb_corpus_embeddings.p", "r"
-        ) as f:
-            return pickle.load(f)
-
-
-# Set up the models
-@st.cache_resource(show_spinner="Load bi-encoder model...")
-def load_biencoder():
-    return SentenceTransformer("/usr/src/app/models/multi-qa-mpnet-base-dot-v1")
-
-
 @st.cache_resource(show_spinner="Load cross-encoder model...")
 def load_crossencoder():
     return CrossEncoder("/usr/src/app/models/stsb-distilroberta-base_plus_easy")
 
 
-# Not in use !!!
-def read_greendb(path):
-    products = pd.read_parquet(path)
-    print(len(products), "products loaded")
-    # Further deduplication, drop deuplicated gtins and asins
-    products_null_id = products[(products.gtin.isna()) & (products.asin.isna())]
-    depuplicated_ids = products.drop_duplicates(subset=["gtin", "asin"], keep="first")
-    products = pd.concat([products_null_id, depuplicated_ids])
-    print(len(products), "products after deduplication")
-    products["categories_str"] = array_to_str(products["categories"])
-    products["color_str"] = array_to_str(products["colors"])
-    products["color_str"] = products["color_str"].fillna("")
-    products["description"] = products["description"].fillna("")
-    products["brand"] = products["brand"].fillna("")
-    products["attributes_concat"] = (
-        products["name"]
-        + products["description"]
-        + products["brand"]
-        + products["color_str"]
-    )
-    return products
-
-
-def array_to_str(column):
-    list_str = []
-    for element in column:
-        if element is not None:
-            for e in element:
-                e_str = str("".join(e))
-        else:
-            e_str = str("")
-        list_str.append(e_str)
-    return list_str
-
-
-def make_clickable(val):
-    return '<a href="{}" target="_blank">{}</a>'.format(val, val)
